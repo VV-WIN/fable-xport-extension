408d16b4be78028e6b65f31d92ce68ad4a45f184
Vivi Kajic <vivi@kajic.com>
2026-03-09 22:30:00 -0700

    Add typescript configurations

---
+++ src/popup.ts
@@ -0,0 +1,12 @@
+document.getElementById("export")!.addEventListener("click", async () => {
+
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  chrome.tabs.sendMessage(tab.id!, {
+    type: "START_EXPORT"
+  });
+
+});
